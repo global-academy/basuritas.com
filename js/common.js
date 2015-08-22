5d14a61b71682c787a9bda22e5a402cbe024e6f3
Mauro Trigo <mauro.trigo@gmail.com>
2015-08-21 18:50:03 -0700

    Moved config

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-Parse.initialize('ShEnSXSPYsKQca8SmN4G83P8x8OoQswSE5xw0x61', 'nEXp2NDj9u6lP19BeOgG2rXIAUpyUXXRutIAphsi');
+Parse.initialize(appConfig.parseApp.id, appConfig.parseApp.key);
 
 var allowedFileExtensions = [
 	'jpg',
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-Parse.initialize('ShEnSXSPYsKQca8SmN4G83P8x8OoQswSE5xw0x61', 'nEXp2NDj9u6lP19BeOgG2rXIAUpyUXXRutIAphsi');
 
 var allowedFileExtensions = [
 	'jpg',
